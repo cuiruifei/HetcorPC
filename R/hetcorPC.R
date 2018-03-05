beae89190de0a56583dbaed6dd095460b7d35d8f
Ruifei Cui <36934954+cuiruifei@users.noreply.github.com>
2018-03-05 23:43:02 +0100

    Create hetcorPC.R

---
+++ R/hetcorPC.R
@@ -0,0 +1,27 @@
+#### 0. Dependent Packages ####
+
+library(polycor)
+library(pcalg)
+source('gaussCItestLocal.R')
+
+#### 1. Read Data ####
+
+## an example
+X <- iris[,-4]
+
+#### 2. Learning Correlation Matrix and Effective Sample Size ####
+
+## inference
+het.obj <- hetcor(X)
+## heterogeneous correlations
+het.corr <- het.obj$correlations
+## standard deviations
+het.sd <- het.obj$std.errors
+## effective sample size
+het.ess <- ((1-het.corr^2)/het.sd)^2
+
+#### 3. Call the PC Algorithm for Causal Discovery ####
+
+##
+graph.het <- pc(suffStat = list(C = het.corr, n = nrow(X), ESS.Mat = het.ess), 
+                indepTest = gaussCItestLocal, alpha = 0.01, p = ncol(X))
